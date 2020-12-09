18adcb10089c1c3d3a0b8eca876090c33d6b7a0c
Sam-Ayn <samueleayn@gmail.com>
2020-12-09 14:00:37 +0300

    Initial commit

---
+++ src/test/java/ru/springcourse/lessons/thymeleafdemo/ThymeleafDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.springcourse.lessons.thymeleafdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
